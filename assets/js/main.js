21d40a13368304a27c92bee89661378e5073e19e
GovindaR <govindaranamagar44@gmail.com>
2021-07-14 12:43:06 +0545

    Add files via upload
    desktop version

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$('.beneficiaries-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    centerMode: true,
+    padding:0,
+    speed:1300,
+    autoplay: true,
+    autoplaySpeed: 2000
+    });
